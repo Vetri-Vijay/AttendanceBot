923b0ca899d1fbc73ff626105adcdedcec55b749
Vetri-Vijay <94090516+Vetri-Vijay@users.noreply.github.com>
2022-10-13 11:24:30 -0400

    Updated Functionality
    - Displays an error when a user does not enter their name with their code
    - Deletes all message from the channel to prevent the code from being shared through incorrect command usage
    - Formates the CSV with users as rows and dates as columns for improved readability

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# Developed by Vetri Vijay and tested By Allen Zhao and Valabh Korivi
+##################################################
+## Software for the AttendenceBot in the MSU Solar
+## Racing Team discord server
+##
+## Author: Vetri Vijay
+## Credits: Allen Zhao and Valabh Korivi
+## Email: vetivijay2002@gmail.com
+##################################################
 
 import os
 from datetime import datetime
@@ -6,6 +13,7 @@ from datetime import datetime
 import discord
 from dotenv import load_dotenv
 import pytz
+import pandas as pd
 
 load_dotenv('.env')
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,6 +24,20 @@ FILE = os.environ.get('DISCORD_FILE')
 CONFIGFILE = os.environ.get('DISCORD_CONFIGFILE')
 
 client = discord.Client(intents=discord.Intents.default())
+
+def add_name(df,name):
+  if name not in df["name"].tolist():
+    df.loc[len(df)] = [name] + [0 for _ in range(df.shape[1]-1)]
+  return df["name"].tolist().index(name)
+
+def add_date(df,date):
+  if date not in df.columns:
+    df[date] = [0 for _ in range(len(df['name']))]
+
+def enter_attendance(df,name,date):
+  a = add_name(df=df,name=name)
+  add_date(df=df,date=date)
+  df.at[a,date] = 1
 
 @client.event
 async def on_ready():
@@ -61,11 +83,16 @@ async def on_message(message):
     if name == "":
       await message.channel.send(f"You forgot your name, add it after the code\n")
     else:
+      tz_city = pytz.timezone('America/Detroit')
+      d1 = datetime.now(tz_city).strftime("%m,%d,%Y")
+      try:
+        df = pd.read_csv('attendencesheet.csv')
+      except:
+        df = pd.DataFrame()
+        df['name'] = []
+      enter_attendance(df=df,name=name,date=d1)
+      df.to_csv('attendencesheet.csv',index=False)
       await message.channel.send(f"{name}'s attendence was recorded\n")
-      with open(FILE, 'a') as file_object:
-        tz_city = pytz.timezone('America/Detroit')
-        d1 = datetime.now(tz_city).strftime("%H,%M,%S,%m,%d,%Y")
-        file_object.write(f"{name.lower()},{d1}\n")
         
   elif command == CONFIGCODE:
     print()
